49d4fc07e3b302f12e6b971f7c9dc132251e149a
Fede Backhaus <fbackhaus94@gmail.com>
2017-06-22 23:38:02 -0300

    Added comments

--- flickrApp/www/js/app.fotos.controller.js
+++ flickrApp/www/js/app.fotos.controller.js
@@ -4,17 +4,18 @@ angular.module('flickrApp')
 		$state.go('app.bienvenido');
 	}
 
-	$scope.callback = function(item) {
+	$scope.showPhoto = function(item) {
 		console.log(item);
 		$scope.imageSrc = item.src;
 		$scope.imageSub = item.sub;
 		$scope.imageId = item.id;
 		$scope.openModal();
+		getComments($scope.imageId);
 	}
 
-	$scope.holis = function() {
-		console.log("JELOU");
-	}
+	$scope.showComments = function() {
+		$scope.openCommentsModal();
+	};
 
 	$ionicModal.fromTemplateUrl('image-modal.html', {
 		scope: $scope,
@@ -35,14 +36,7 @@ angular.module('flickrApp')
     $scope.$on('$destroy', function() {
     	$scope.modal.remove();
     });
-    // Execute action on hide modal
-    $scope.$on('modal.hide', function() {
-      // Execute action
-  });
-    // Execute action on remove modal
-    $scope.$on('modal.removed', function() {
-      // Execute action
-  });
+
     $scope.$on('modal.shown', function() {
     	console.log('Modal is shown!');
     });
@@ -50,7 +44,31 @@ angular.module('flickrApp')
     function getComments(photoId) {
     	flickrApiSvc.getComments(photoId)
     	.then(function(comments) {
-    		console.log(comments);
+    		$scope.comments = comments.comments.comment;
     	})
     }
+
+    $ionicModal.fromTemplateUrl('comments-modal.html', {
+    	scope: $scope,
+    	animation: 'slide-in-up'
+    }).then(function(modal) {
+    	$scope.commentsModal = modal;
+    });
+
+    $scope.openCommentsModal = function() {
+    	$scope.commentsModal.show();
+    };
+
+    $scope.closeCommentsModal = function() {
+    	$scope.commentsModal.hide();
+    };
+
+    //Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function() {
+    	$scope.commentsModal.remove();
+    });
+    
+    $scope.$on('commentsModal.shown', function() {
+    	console.log('Comments Modal is shown!');
+    });
 });
